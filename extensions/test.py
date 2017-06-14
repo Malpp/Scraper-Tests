924f563a58914c5d8deb45e7c37566c316f49f32
malpp <malpp@users.noreply.github.com>
2017-06-14 09:46:37 -0400

    init

---
+++ extensions/test.py
@@ -0,0 +1,7 @@
+# Title: Hello world!
+# Title: Foo bar...
+# Description: This is a test
+# Description: to see if the extensions
+# Description: scraper works with github
+# Required file: Original file, Body text
+# Required file: Thumbnail
